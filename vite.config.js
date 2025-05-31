5d0ae83adb43291cab80d22cf30a11622d385e1f
Santu-kumar364 <santukumar7619@gmail.com>
2025-05-31 13:10:03 +0530

    add vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/XzalChat/',  // 👈 This is essential for GH Pages deployment!
 })
